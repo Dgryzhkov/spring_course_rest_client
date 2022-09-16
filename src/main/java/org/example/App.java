2402457bf4182a773da64570eb26d52f94f250a9
Denis Ryzhkov <dgryzhkov@gmail.com>
2022-09-16 09:29:50 +0700

    rest client done!

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,33 @@
 package org.example;
 
+import org.example.configuration.MyConfig;
+import org.example.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.servlet.support.JstlUtils;
+
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(MyConfig.class);
+
+        Communication communication = context.getBean("communication", Communication.class);
+//        List<Employee>  allEmployees = communication.getAllEmployees();
+//        System.out.println(allEmployees);
+
+//        Employee empByOd = communication.getEmployee(2);
+//        System.out.println(empByOd);
+
+//        Employee emp = new Employee("Svetlana", "Sokolova", "IT", 12000);
+//        emp.setId(10);
+//
+//        communication.saveEmployee(emp);
+
+        communication.deleteEmployee(10);
     }
 }
